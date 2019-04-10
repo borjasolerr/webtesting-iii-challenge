ea470ef8e51bf7530d061eecce87d466b2c1f157
borjasolerr <borjasolrey@gmail.com>
2019-04-10 16:17:11 +0200

    Added tests for Controls component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,33 @@
 // Test away!
+import React from 'react';
+import * as rtl from 'react-testing-library';
+
+import Controls from './Controls';
+
+afterEach(rtl.cleanup);
+
+describe('Controls component', () => {
+  it('renders Lock Gate and Close Gate buttons succesfully', () => {
+    const wrapper = rtl.render(<Controls />);
+    expect(wrapper.queryByText('Lock Gate')).toBeTruthy();
+    expect(wrapper.queryByText('Close Gate')).toBeTruthy();
+  });
+
+  it('renders Open Gate and Unlock Gate buttons succesfully', () => {
+    const wrapper = rtl.render(<Controls locked={true} closed={true} />);
+    expect(wrapper.queryByText('Open Gate')).toBeTruthy();
+    expect(wrapper.queryByText('Unlock Gate')).toBeTruthy();
+  });
+
+  it('disables Lock Gate button if the gate is opened', () => {
+    const wrapper = rtl.render(<Controls locked={false} closed={false} />);
+    rtl.fireEvent.click(wrapper.queryByText('Lock Gate'));
+    expect(wrapper.queryByText('Unlock Gate')).toBeFalsy();
+  });
+
+  it('disables Open Gate button if the gate is locked', () => {
+    const wrapper = rtl.render(<Controls locked={true} closed={true} />);
+    rtl.fireEvent.click(wrapper.queryByText('Open Gate'));
+    expect(wrapper.queryByText('Close Gate')).toBeFalsy();
+  });
+});
